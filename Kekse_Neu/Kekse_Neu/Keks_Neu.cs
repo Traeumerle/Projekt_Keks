2d71b8b6929824e88224f11d3045d5ad56dcd248
Toni <Toni5416@aol.com>
2018-04-09 16:06:21 +0200

    Methoden in Keks_Neu implementiert

--- Kekse_Neu/Kekse_Neu/Keks_Neu.cs
+++ Kekse_Neu/Kekse_Neu/Keks_Neu.cs
@@ -8,5 +8,19 @@ namespace Kekse_Neu
 {
     public class Keks_Neu
     {
+        public static List<int> Start_Zahlen_testen(int untergrenze, int obergrenze)
+        {
+            var zahlen = Zahlen_testen(untergrenze, obergrenze);
+            return zahlen;
+        }
+        internal static List<int> Zahlen_testen(int untergrenze, int obergrenze)
+        {
+            var zahlen = new List<int>();
+            for (int zahl = untergrenze; zahl <= obergrenze; zahl++)
+            {
+                zahlen.Add(zahl);
+            }
+            return zahlen;
+        }
     }
 }
