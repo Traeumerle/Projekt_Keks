c1a1eeff93360a3f62c619697af56b384e1b0788
Toni <Toni5416@aol.com>
2018-04-09 15:53:52 +0200

    Neue Testklasse angelegt
    unsure

--- Kekse_Neu/KlasseTest/Properties/AssemblyInfo.cs
+++ Kekse_Neu/KlasseTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Kekse_Neu")]
+[assembly: AssemblyTitle("KlasseTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kekse_Neu")]
+[assembly: AssemblyProduct("KlasseTest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("9891cc2e-28fe-4421-8120-3ab3f2424d2d")]
+[assembly: Guid("42f3cf12-8978-46fd-8f14-10b1f30823e0")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("KlasseTest")]
