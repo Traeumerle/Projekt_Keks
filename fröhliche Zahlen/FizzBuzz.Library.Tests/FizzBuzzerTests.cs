c47e655550b74581f071f7259c8a8664ac7e12e3
Toni <Toni5416@aol.com>
2018-04-11 10:13:43 +0200

    - comments
    little

--- fröhliche Zahlen/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
+++ fröhliche Zahlen/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -33,36 +33,5 @@ namespace FizzBuzz.Library.Tests
             //Assert
             Assert.AreEqual(output, "4");
         }
-
-
-
-        //    [Test]
-        //    public void Buzzer_WhenDefault_ReturnsInput([Values(1,2,4,7,8,11,13,14,16,17,19)]int input)
-        //    {
-        //        //Arrange
-        //        //Act
-        //        string output = FizzBuzzer.GetValue(input);
-        //        //Assert
-        //        Assert.AreEqual(input.ToString(), output);
-        //    }
-
-        //    [Test]
-        //    public void Fizz_Whendiv3_ReturnsFizz([Values(3,6,9,12,18)]int input)
-        //    {
-        //        string output = FizzBuzzer.GetValue(input);
-
-        //        Assert.AreEqual("Fizz", output);
-        //    }
-
-        //    [Test]
-        //    public void Fizz_Whendiv5_ReturnsBuzz([Values(5,10)]int input)
-        //    {
-
-
-        //        string output = FizzBuzzer.GetValue(input);
-
-        //        Assert.AreEqual("Buzz", output);
-        //    }
-        //}
     }
 }
