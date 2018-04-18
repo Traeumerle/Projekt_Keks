b482d6a602ce51b4050278fea7caa3431a37acbf
Toni <Toni5416@aol.com>
2018-04-18 09:50:26 +0200

    tryout

--- russische_Bauermultiplikation_Code/russische_Bauermultiplikation_Code/Class1.cs
+++ russische_Bauermultiplikation_Code/russische_Bauermultiplikation_Code/Class1.cs
@@ -8,19 +8,26 @@ namespace russische_Bauermultiplikation_Code
 {
     public class Class1
     {
+        public static void Main(string[] args)
+        {
+
+            var x = russische_Bauern_übersetzen(47, 42);
+            Russisch_ausgeben(x);
+        }
 
         public static void Russisch_ausgeben(Dictionary<int, int> Kekse)
         {
-            foreach(KeyValuePair<int, int> item in Kekse)
+            foreach (KeyValuePair<int, int> item in Kekse)
             {
+
                 Console.WriteLine(item.Value);
+
             }
         }
 
         public static int X_Berechnung(int x)
         {
-            x = x - x % 2;
-            x = x / 2;
+            x = (x - x % 2) / 2;
             return x;
         }
         public static int Y_Berechnung(int y)
@@ -28,21 +35,47 @@ namespace russische_Bauermultiplikation_Code
             y = y * 2;
             return y;
         }
-        public static int Multipliziere(int x, int y)
+
+        internal static Dictionary<int, int> russische_Bauern_übersetzen(int x, int y)
         {
-            int rückgabeX = x ;
-            return rückgabeX;
+
+
+            return russische_Bauern(x, y);
         }
+
+
+        internal static int ersterWert(int x, int y)
+        {
+
+            return ersterWert(x, y);
+        }
+
         public static Dictionary<int, int> russische_Bauern(int x, int y)
         {
             Dictionary<int, int> Russisch = new Dictionary<int, int>();
-            do
+
+
+            if (x % 2 != 0)
             {
-                x = X_Berechnung(x);
-                y = Y_Berechnung(y);
                 Russisch.Add(x, y);
-            } while (x != 1);
+                do
+                {
+
+                    x = X_Berechnung(x);
+                    y = Y_Berechnung(y);
+                    if (x % 2 != 0)
+                    {
+                        Russisch.Add(x, y);
+                    }
+                } while (x != 1);
+            }
             return Russisch;
+        }
+
+        public void x_verändern(int x)
+        {
+            x = x - x % 2;
+            x = x / 2;
         }
     }
 }
