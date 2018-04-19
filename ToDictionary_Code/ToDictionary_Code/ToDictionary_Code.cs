c67c3551ad82fb4e50466b19b6b05cca324c9d46
Toni <Toni5416@aol.com>
2018-04-19 16:12:11 +0200

    ToDictionary - getting started

---
+++ ToDictionary_Code/ToDictionary_Code/ToDictionary_Code.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDictionary_Code
+{
+    public class ToDictionary_Code
+    {
+
+        public static Dictionary<string, string>Dicterstellen(string x)
+        {
+            Dictionary<string, string> ToDictionary = new Dictionary<string, string>();
+
+            string uebersetztes_x = Uebersetze_DictionaryCase1(x);
+            ToDictionary.Add(x, uebersetztes_x);
+
+            return ToDictionary;
+        }
+
+        public static string Uebersetze_DictionaryCase1(string x)
+        {
+            string[] split_string = x.Split(new Char[] { ';'});
+            return Convert.ToString(split_string);
+        }
+
+        public static void Ausgabe(string x, Dictionary<string, string> Kekse)
+        {
+            foreach (var item in Kekse)
+            {
+                Console.WriteLine(Dicterstellen(x));
+            }
+            
+        }
+
+    }
+}
---
+++ ToDictionary_Code/ToDictionary_Test/ToDictionary_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDictionary_Test
+{
+    public class ToDictionary_Test
+    {
+
+
+    }
+}
